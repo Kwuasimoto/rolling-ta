6cde10b769c05a6db632d4fa83ceab9ba905456a
Kwuasimoto <thomasshankgit@gmail.com>
2024-09-20 04:48:42 -0400

    python is on drugs

--- main.py
+++ main.py
@@ -4,11 +4,30 @@ from rolling_ta.data import CSVLoader, XLSXLoader, XLSXWriter
 from rolling_ta.momentum import NumbaStochasticRSI, NumbaRSI
 from ta.momentum import StochRSIIndicator, RSIIndicator
 from rolling_ta.logging import logger
+from rolling_ta.volatility.tr import NumbaTrueRange
 
 if __name__ == "__main__":
-    logger.info(not np.isclose(1.000999, 1.000119, atol=1e-6))
+    # logger.info(not np.isclose(1.000999, 1.000119, atol=1e-6))
     # loader = CSVLoader()
     # btc = loader.read_resource()
+
+    loader = XLSXLoader()
+    atr_df = loader.read_resource(
+        "btc-atr.xlsx",
+        columns=[
+            "ts",
+            "high",
+            "low",
+            "close",
+            "tr",
+            "atr",
+        ],
+    ).copy()
+
+    tr = NumbaTrueRange(atr_df.iloc[:40])
+
+    for i, series in atr_df.iloc[40:].iterrows():
+        tr.update(series)
 
     # writer = XLSXWriter("btc-sma.xlsx")
 
